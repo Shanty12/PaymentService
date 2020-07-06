4de7a52075f2dc8853ee238e5161ddcf4d54a975
shanty.j <shanty.j@pitsolutions.com>
2020-07-06 22:36:15 +0530

    fix

--- AuthenticationAdapterInterface.php
+++ AuthenticationAdapterInterface.php
@@ -25,54 +25,24 @@
 
 namespace Saferpay\PaymentService;
 
-interface Constants
+interface AuthenticationAdapterInterface
 {
     /**
-     * API constants
+     * generate Authorization header data
+     *
+     * @return array
      */
-    const API_SPEC_VERSION = '1.16';
+    public function generateAuthorizationheader();
 
-    const API_ACCEPT_HEADER_TYPE = 'application/json';
-
-    const API_CHARSET = 'utf-8';
-
-    const API_TIMEOUT = 30;
-
-    const API_RETRY_INDICATOR = 0;
-
-    const API_ALIAS_INSERT = 'api/Payment/v1/Alias/Insert';
-
-    const API_ALIAS_GET_DETAILS = 'api/Payment/v1/Alias/AssertInsert';
-
-    const API_DEFAULT_SHOP_NAME = 'Magento Shop';
-
-    const API_DEFAULT_LANG_CODE = 'en';
-
-    const API_PAYMENT_LIFE_TIME = 1000;
-
-    const API_ALIAS_ID_GENERATOR = 'RANDOM_UNIQUE';
-
-    const ACTIVE = 1;
-
-    const INACTIVE = 0;
-
-    const CARD_HOLDER_NAME_NONE = 'NONE';
-
-    const SAFERPAY_MASTERPASS_WALLET = 'MASTERPASS';
-
-    const SAFERPAY_APPLEPAY_WALLET = 'APPLEPAY';
-
-    const API_ACTION_CAPTURE = 'capture';
-
-    const API_ACTION_REFUND = 'refund';
-
-    const PAYMENT_PAGE = 'PaymentPage';
-
-    const TRANSACTION_PAGE = 'Iframe';
-
-    const TRUE = true;
-
-    const FORCE = 'FORCE';
-
-    const API_ALIAS_AUTHENTICATION_ONLINE = 'ONLINE_STRONG';
+    /**
+     * send Request
+     *
+     * @param array $headerData
+     * @param array $bodyFormData
+     * @param string $url
+     * @param string $username
+     * @param string $password
+     * @return array
+     */
+    public function sendRequest($headerData, $bodyFormData, $url, $username, $password);
 }
--- RefundAdapterInterface.php
+++ RefundAdapterInterface.php
@@ -25,54 +25,37 @@
 
 namespace Saferpay\PaymentService;
 
-interface Constants
+interface RefundAdapterInterface
 {
     /**
-     * API constants
+     * Function to build Initialize API body Data
+     *
+     * @param array $bodyData
+     * @return array
      */
-    const API_SPEC_VERSION = '1.16';
+    public function buildRefundBodyData($bodyData);
 
-    const API_ACCEPT_HEADER_TYPE = 'application/json';
+    /**
+     * Function to build Refund Capture API body Data
+     *
+     * @param array $bodyData
+     * @return array
+     */
+    public function buildRefundCaptureData($bodyData);
 
-    const API_CHARSET = 'utf-8';
+    /**
+     * Function to build Refund Cancel API body Data
+     *
+     * @param array $bodyData
+     * @return array
+     */
+    public function buildRefundCancelData($bodyData);
 
-    const API_TIMEOUT = 30;
-
-    const API_RETRY_INDICATOR = 0;
-
-    const API_ALIAS_INSERT = 'api/Payment/v1/Alias/Insert';
-
-    const API_ALIAS_GET_DETAILS = 'api/Payment/v1/Alias/AssertInsert';
-
-    const API_DEFAULT_SHOP_NAME = 'Magento Shop';
-
-    const API_DEFAULT_LANG_CODE = 'en';
-
-    const API_PAYMENT_LIFE_TIME = 1000;
-
-    const API_ALIAS_ID_GENERATOR = 'RANDOM_UNIQUE';
-
-    const ACTIVE = 1;
-
-    const INACTIVE = 0;
-
-    const CARD_HOLDER_NAME_NONE = 'NONE';
-
-    const SAFERPAY_MASTERPASS_WALLET = 'MASTERPASS';
-
-    const SAFERPAY_APPLEPAY_WALLET = 'APPLEPAY';
-
-    const API_ACTION_CAPTURE = 'capture';
-
-    const API_ACTION_REFUND = 'refund';
-
-    const PAYMENT_PAGE = 'PaymentPage';
-
-    const TRANSACTION_PAGE = 'Iframe';
-
-    const TRUE = true;
-
-    const FORCE = 'FORCE';
-
-    const API_ALIAS_AUTHENTICATION_ONLINE = 'ONLINE_STRONG';
+    /**
+     * Function to build Refund Assert API body Data
+     *
+     * @param array $bodyData
+     * @return array
+     */
+    public function buildAssertRefundData($bodyData);
 }
